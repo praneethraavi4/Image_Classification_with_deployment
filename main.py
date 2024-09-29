d973e4557407af0c0f32ec146bb60a3e89eb4589
sai <saipraneethravi0409@gmail.com>
2024-09-29 09:48:06 +0200

    create training script

---
+++ main.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers
+import matplotlib.pyplot as plt
+
+IMAGE_SIZE = 256
+BATCH_SIZE = 32
+EPOCHS = 5
+
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "data", shuffle=True,
+    image_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE)
+
+class_names = dataset.class_names
+
+def split_data(ds,train_split, test_split,val_split):
+    length = len(ds)
+    train = int(length*train_split)
+    train_ds = ds.take(train)
+    test = int(length*test_split)
+    val = int(length*val_split)
+    val_ds = ds.skip(train).take(val)
+    test_ds = ds.skip(train).skip(val)
+    return train_ds, test_ds, val_ds
+
+train_ds, test_ds, val_ds =  split_data(dataset,0.8,0.1,0.1)
+
+print(len(train_ds))
+print(len(test_ds))
+print(len(val_ds))
+
+train_ds = train_ds.cache().shuffle(10000).prefetch(buffer_size = tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(10000).prefetch(buffer_size = tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(10000).prefetch(buffer_size = tf.data.AUTOTUNE)
+
+rescale_resize = tf.keras.Sequential([
+    layers.Resizing(IMAGE_SIZE,IMAGE_SIZE),
+    layers.Rescaling(1.0/255)
+])
+
+data_augmentation = tf.keras.Sequential([
+    layers.RandomRotation(0.2),
+    layers.RandomFlip(),
+    layers.RandomTranslation(height_factor = 0.3, width_factor = 0.4)
+])
+
+img_shape = (256,256, 3)  # height, width, channels
+
+model = tf.keras.models.Sequential([
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=img_shape),  # Input shape here
+    layers.MaxPool2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.MaxPool2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.MaxPool2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation="relu"),
+    layers.MaxPool2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(3, activation="softmax"),
+])
+
+model.summary()
+model.compile(
+    optimizer = "Adam",
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False),
+    metrics = ["accuracy"]
+)
+
+history =  model.fit(train_ds,epochs = EPOCHS, batch_size = BATCH_SIZE, verbose = 1, validation_data = val_ds)
